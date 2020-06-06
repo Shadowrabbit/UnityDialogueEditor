c96d26e8d178378e3aaf06c46f7a100020984136
JosephBarber96 <Joseph_Barber@Hotmail.co.uk>
2020-06-06 16:59:06 +0100

    Added new offline documentation

--- DialogueEditor/Assets/DialogueEditor/Assets/Scripts/NPCConversation.cs
+++ DialogueEditor/Assets/DialogueEditor/Assets/Scripts/NPCConversation.cs
@@ -243,30 +243,37 @@ namespace DialogueEditor
     public class SpeechNode : ConversationNode
     {
         public string Name;
+
         /// <summary>
         /// Should this speech node go onto the next one automatically?
         /// </summary>
         public bool AutomaticallyAdvance;
+
         /// <summary>
         /// Should this speech node display a "continue" or "end" option?
         /// </summary>
         public bool AutoAdvanceShouldDisplayOption;
+
         /// <summary>
         /// If AutomaticallyAdvance==True, how long should this speech node 
         /// display before going onto the next one?
         /// </summary>
         public float TimeUntilAdvance;
+
         public Sprite Icon;
         public AudioClip Audio;
         public float Volume;
+
         /// <summary>
         /// The Options available on this Speech node, if any.
         /// </summary>
         public List<OptionNode> Options;
+
         /// <summary>
         /// The Speech node following the current, if any.
         /// </summary>
         public SpeechNode Dialogue;
+
         public UnityEngine.Events.UnityEvent Event;
     }
 
